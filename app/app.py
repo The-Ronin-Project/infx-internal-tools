e3bc908b37be0c22a970e63e5ffeecd7ff511c9f
anneH <anne@projectronin.com>
2021-12-09 13:43:08 -0800

    Adding ddtrace import

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from io import StringIO
 from flask import Flask, jsonify, request, Response
 from app.models.value_sets import *
 from app.models.surveys import *
+from ddtrace import patch_all
+patch_all()
 
 app = Flask(__name__)
 app.config['MOCK_DB'] = False
