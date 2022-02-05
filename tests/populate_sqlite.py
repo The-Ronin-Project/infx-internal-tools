19a8132b40e77321cce6dc8e2d963570f538196e
Rey Johnson <rey@projectronin.com>
2022-02-04 17:19:39 -0700

    Add test terminology data for LOINC

--- tests/populate_sqlite.py
+++ tests/populate_sqlite.py
@@ -9,21 +9,23 @@ cursor.execute("attach database 'dbs/icd_10_cm.db' as icd_10_cm")
 cursor.execute("attach database 'dbs/rxndirty.db' as rxnormDirty")
 cursor.execute("attach database 'dbs/surveys.db' as surveys")
 cursor.execute("attach database 'dbs/organizations.db' as organizations")
+cursor.execute("attach database 'dbs/loinc.db' as loinc")
 
 sql_files = [
-    "sql_exports/term_versions.sql", 
-    "sql_exports/snomed_schema.sql", 
-    "sql_exports/icd10cm.sql", 
-    "sql_exports/rxndirty.sql",
-    "sql_exports/surveys.sql",
+    # "sql_exports/term_versions.sql", 
+    # "sql_exports/snomed_schema.sql", 
+    # "sql_exports/icd10cm.sql", 
+    # "sql_exports/rxndirty.sql",
+    # "sql_exports/surveys.sql",
+    "sql_exports/loinc.sql"
     # Value Sets
-    "sql_exports/value_set.sql",
-    "sql_exports/vs_version.sql",
-    "sql_exports/value_set_rule.sql",
-    "sql_exports/vs_expansion_member.sql",
-    "sql_exports/vs_extensional_member.sql",
-    "sql_exports/vs_expansion.sql",
-    "sql_exports/organizations.sql",
+    # "sql_exports/value_set.sql",
+    # "sql_exports/vs_version.sql",
+    # "sql_exports/value_set_rule.sql",
+    # "sql_exports/vs_expansion_member.sql",
+    # "sql_exports/vs_extensional_member.sql",
+    # "sql_exports/vs_expansion.sql",
+    # "sql_exports/organizations.sql",
     ]
 
 for sql_path in sql_files:
