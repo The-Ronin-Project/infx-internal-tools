7fa32397d41efa466b7123c47736ed237fc1fdd2
Rey Johnson <rey@projectronin.com>
2022-09-20 16:22:40 -0600

    Adjust results so SCD, SBD, GPCK, and BPCK are always returned

--- app/models/rxnorm.py
+++ app/models/rxnorm.py
@@ -43,7 +43,12 @@ def approx_search(query_string):
     top_rxcui = rxcuis[0]
     rxcuis = list(set(rxcuis))
 
-    final_results = {}
+    final_results = {
+        'SCD': [],  # Make sure the term types we care about always return
+        'SBD': [],
+        'GPCK': [],
+        'BPCK': []
+    }
     for rxcui in rxcuis:
         info_request = requests.get(f'{RX_NAV_BASE_URL}/RxTerms/rxcui/{rxcui}/allinfo.json').json()
         if info_request:
