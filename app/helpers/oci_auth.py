8e5b49bba7dc7d109167c2cb08581a3ec1d546a0
azpr8 <azita@projectronin.com>
2022-09-29 16:10:52 -0600

    update, config call works now

--- app/helpers/oci_auth.py
+++ app/helpers/oci_auth.py
@@ -4,25 +4,13 @@ from decouple import config
 from oci.object_storage import ObjectStorageClient
 
 
-def is_oci():
-    oci_config = {
-        "USER": config("OCI_USER"),
-        "FINGERPRINT": config("OCI_FINGERPRINT"),
-        "KEY_FILE": config("OCI_KEY_FILE"),
-        "TENANCY": config("OCI_TENANCY"),
-        "REGION": config("OCI_REGION"),
-    }
-    return oci_config
-
-
 def oci_authentication():
-    oci_vars = is_oci()
     oci_config = {
-        "user": oci_vars["USER"],
-        "fingerprint": oci_vars["FINGERPRINT"],
-        "key_file": oci_vars["KEY_FILE"],
-        "tenancy": oci_vars["TENANCY"],
-        "region": oci_vars["REGION"],
+        "user": config("OCI_USER"),
+        "fingerprint": config("OCI_FINGERPRINT"),
+        "key_file": config("OCI_KEY_FILE"),
+        "tenancy": config("OCI_TENANCY"),
+        "region": config("OCI_REGION"),
     }
     oci.config.validate_config(oci_config)
     object_storage_client = ObjectStorageClient(oci_config)
