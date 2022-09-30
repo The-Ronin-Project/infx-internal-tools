294faa66e60c1257797661640bdec2002566ddd7
azpr8 <azita@projectronin.com>
2022-09-30 08:38:36 -0600

    more updates

--- app/helpers/oci_auth.py
+++ app/helpers/oci_auth.py
@@ -6,11 +6,11 @@ from oci.object_storage import ObjectStorageClient
 
 def oci_authentication():
     oci_config = {
-        "user": config("OCI_USER"),
-        "fingerprint": config("OCI_FINGERPRINT"),
-        "key_file": config("OCI_KEY_FILE"),
-        "tenancy": config("OCI_TENANCY"),
-        "region": config("OCI_REGION"),
+        "user": config("OCI_CLI_USER"),
+        "fingerprint": config("OCI_CLI_FINGERPRINT"),
+        "key_file": config("OCI_CLI_KEY_FILE"),
+        "tenancy": config("OCI_CLI_TENANCY"),
+        "region": config("OCI_CLI_REGION"),
     }
     oci.config.validate_config(oci_config)
     object_storage_client = ObjectStorageClient(oci_config)
