c5376f6a21b4d3d20f94958447a5b83caf91d730
Rey Johnson <rey@projectronin.com>
2023-03-22 09:51:35 -0600

    Set default values for database string so test runners do not fail

--- app/database.py
+++ app/database.py
@@ -3,15 +3,18 @@ from sqlalchemy import create_engine
 from elasticsearch import Elasticsearch
 from decouple import config
 
-SQL_ALCHEMY_ENGINE = create_engine(f"postgresql://{config('DATABASE_USER')}@{config('DATABASE_HOST')}:{config('DATABASE_PASSWORD')}@{config('DATABASE_HOST')}/{config('DATABASE_NAME')}",
-                                   connect_args={'sslmode':'require'},
-                                   pool_size=2,
-                                   max_overflow=0)
+SQL_ALCHEMY_ENGINE = create_engine(
+    f"postgresql://{config('DATABASE_USER', default='')}@{config('DATABASE_HOST', default='')}:{config('DATABASE_PASSWORD', default='')}@{config('DATABASE_HOST', default='')}/{config('DATABASE_NAME', default='')}",
+    connect_args={"sslmode": "require"},
+    pool_size=2,
+    max_overflow=0,
+)
+
 
 def get_db():
-    if 'db' not in g:
-        if current_app.config['MOCK_DB'] is True:
-            engine = create_engine('sqlite:///tests/dbs/public.db')
+    if "db" not in g:
+        if current_app.config["MOCK_DB"] is True:
+            engine = create_engine("sqlite:///tests/dbs/public.db")
             g.db = engine.connect()
             g.db.execute("attach database 'tests/dbs/concept_maps.db' as concept_maps")
             g.db.execute("attach database 'tests/dbs/value_sets.db' as value_sets")
@@ -21,18 +24,24 @@ def get_db():
             g.db.execute("attach database 'tests/dbs/rxndirty.db' as rxnormDirty")
             g.db.execute("attach database 'tests/dbs/surveys.db' as surveys")
             g.db.execute("attach database 'tests/dbs/loinc.db' as loinc")
-            g.db.execute("attach database 'tests/dbs/organizations.db' as organizations")
+            g.db.execute(
+                "attach database 'tests/dbs/organizations.db' as organizations"
+            )
         else:
             g.db = SQL_ALCHEMY_ENGINE.connect()
     return g.db
 
+
 def get_elasticsearch():
-    if 'es' not in g:
-        g.es = Elasticsearch(f"https://{config('ELASTICSEARCH_USER')}:{config('ELASTICSEARCH_PASSWORD')}@{config('ELASTICSEARCH_HOST')}/")
+    if "es" not in g:
+        g.es = Elasticsearch(
+            f"https://{config('ELASTICSEARCH_USER')}:{config('ELASTICSEARCH_PASSWORD')}@{config('ELASTICSEARCH_HOST')}/"
+        )
     return g.es
 
+
 def close_db(e=None):
-    db = g.pop('db', None)
+    db = g.pop("db", None)
 
     if db is not None:
         db.close()
