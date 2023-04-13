63a26e7eebc444c16160780a61c87e80189939e2
tere646 <theresa@projectronin.com>
2023-04-13 16:38:23 -0600

    testing env variables

--- app/helpers/simplifier_helper.py
+++ app/helpers/simplifier_helper.py
@@ -9,9 +9,8 @@ import requests
 
 
 # Use config() to read values from the .env file
-# BASE_URL = config("SIMPLIFIER_BASE_URL")
-BASE_URL = config("DATABASE_HOST", default="https://fhir.simplifier.net")
-TOKEN_BASE_URL = config("DATABASE_HOST", default="https://api.simplifier.net/token")
+BASE_URL = config("SIMPLIFIER_BASE_URL")
+TOKEN_BASE_URL = config("SIMPLIFIER_TOKEN_URL")
 
 
 def get_access_token():
