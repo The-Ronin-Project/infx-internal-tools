b313b1d2d0bbebee5e433a21cf38a04384d46e8e
Rey Johnson <rey@projectronin.com>
2023-07-19 14:11:19 -0600

    Have constructor method return instance of concept map

--- app/concept_maps/views.py
+++ app/concept_maps/views.py
@@ -126,7 +126,7 @@ def create_initial_concept_map_and_version_one():
             source_value_set_version_uuid=source_value_set_version_uuid,
             target_value_set_version_uuid=target_value_set_version_uuid,
         )
-        return jsonify(ConceptMap.serialize(new_cm))
+        return jsonify(new_cm.serialize())
     elif request.method == "GET":
         concept_map_uuid = request.values.get("concept_map_uuid")
         version = request.values.get("version")
