a15b2a2acbad9a85b247db4b0b9090b56464cbe0
Theresa <72846467+tere646@users.noreply.github.com>
2023-11-09 16:12:12 -0600

    add catch for ValueSet load uuid none or invalid, remove file from draft_tests/value/sets, add file to tests/value_sets

--- tests/value_sets/test_value_sets.py
+++ tests/value_sets/test_value_sets.py
@@ -3,6 +3,7 @@ import app.value_sets.models
 import app.terminologies.models
 import app.models.codes
 from app.database import get_db
+from werkzeug.exceptions import NotFound
 
 
 class ValueSetTests(unittest.TestCase):
@@ -15,12 +16,17 @@ class ValueSetTests(unittest.TestCase):
 
     def test_value_set_expand(self):
         """
-        Expand the 'Automated Testing Value Set' value set and verify the outputs
+        Expand the 'Automated Testing Value Set' value set and verify the outputs,
+        catch lower level error if value set version uuid isn't valid
         """
         value_set_version_uuid = "58e792d9-1264-4f18-b16e-6292cb7ca597"
+        value_set_version_uuid_2 = "58e792d9-1264-4f18-b16e-0000cb0ca000"
         value_set_version = app.value_sets.models.ValueSetVersion.load(
             value_set_version_uuid
         )
         value_set_version.expand(force_new=True)
 
         self.assertEqual(11509, len(value_set_version.expansion))
+
+        with self.assertRaises(NotFound):
+            app.value_sets.models.ValueSetVersion.load(value_set_version_uuid_2)
