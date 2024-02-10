381ca1f6d36ba03d903ca70aba4d8b8e807504ec
Susan Korgen <susan@projectronin.com>
2024-02-10 16:51:38 -0500

    Rename normalized_code_id to deduplication_hash

--- app/util/concept_map_v4_code_id_column.py
+++ app/util/concept_map_v4_code_id_column.py
@@ -17,14 +17,15 @@ from app.helpers.id_helper import generate_code_id
 LOGGER = logging.getLogger()
 
 
-def identify_duplicate_code_id_values_in_v4(
+def identify_duplicate_code_values_in_v4(
         concept_map_uuid: str = None,
-        concept_map_version_uuid: str = None,
-        output_table_name: str = None,
-        output_pkey_distinct_constraint_name: str = None,
+        concept_map_version_uuid: str = None
 ):
     """
-    Populates a new normalized_code_id column in custom_terminologies.code with values that Systems can leverage to:
+    See app.util.data_migration.ConceptMapsForContent and app.util.data_migration.ConceptMapsForSystems for UUIDs
+    for concept maps. Query the current clinical-content database for "active" and "pending" versions of each.
+
+    Populates a new deduplication_hash column in custom_terminologies.code with values that Systems can leverage to:
 
     STOP THIS CYCLE:
     1. Mapping Request Service has no way to screen incoming codes for what will eventually turn out to be duplicates
@@ -34,18 +35,18 @@ def identify_duplicate_code_id_values_in_v4(
     5 there is no "diff" possible for v4 ConceptMaps (will be in v5) so no one sees this happened until ingestion fails
 
     EASY TO FIX:
-    only after the new normalized_code_id column is populated in custom_terminologies.code, impossible without it
+    only after the new deduplication_hash column is populated in custom_terminologies.code, impossible without it
 
     APPROACH:
-    1. Add normalized_code_id column to code table. Populate the column using this function.
-    2. The Mapping Request Service calculates the normalized_code_id on each incoming code/display pair and if that
-      normalized_code_id is already present in custom_terminologies.code AND ALREADY MAPPED in the relevant concept map
+    1. Add deduplication_hash column to code table. Populate the column using this function.
+    2. The Mapping Request Service calculates the deduplication_hash on each incoming code/display pair and if that
+      deduplication_hash is already present in custom_terminologies.code AND ALREADY MAPPED in the relevant concept map
       for that resource_type, element, tenant, value set, terminology, and code, no work request for Content is created.
-      (Only the normalized_code_id gives us the data to check this rapidly from within Mapping Request Service.
+      (Only the deduplication_hash gives us the data to check this rapidly from within Mapping Request Service.
     3. A "to do" comment in mapping_request_service.py has been added where this would need to be placed (see open PR)
     4. Once the duplication from data ingestion is stopped, de-duplication of existing concept maps could occur.
        concept_map_duplicate_codes.py can output a review spreadsheet for Content (see open PR) to enable these steps:
-    5. A "diff" for ConceptMap artifacts is possible when you order the serialization of rows by normalized_code_id.
+    5. A "diff" for ConceptMap artifacts is possible when you order the serialization of rows by deduplication_hash.
        The rationale for the order will not be human-visible but "diffs" will be TRIVIAL TO SEE once rows are ordered
        in the before and after. We need not build a Retool page immediately: any text "diff" program can operate on JSON
        files as text. Note: we would want to re-output ConceptMap artifacts once before de-dup, and once after, to show
@@ -53,15 +54,15 @@ def identify_duplicate_code_id_values_in_v4(
 
     REVIEW of concept_map_duplicate_codes.py spreadsheet:
     Use with Content review to select which duplicates in concept_map.concept_relationship table are best to keep:
-    1. Systems: all rows where normalized_code_id is unique across the code table - these rows can be auto-marked KEEP
-    2. Systems: where normalized_code_id has duplicates - those with IDENTICAL TARGETS ONLY - 1 can be auto-marked KEEP
-    3. Systems: use the concept_map_duplicate_codes.py function to use this normalized_code_id
-    3. select * ... order by normalized_code_id, download the output as a CSV file, open in Excel, save as XLSX format
-    4. Content: in Excel,marks the 1 KEEP decision for each normalized_code_id group in that empty "fix_action" column
+    1. Systems: all rows where deduplication_hash is unique across the code table - these rows can be auto-marked KEEP
+    2. Systems: where deduplication_hash has duplicates - those with IDENTICAL TARGETS ONLY - 1 can be auto-marked KEEP
+    3. Systems: use the concept_map_duplicate_codes.py function to use this deduplication_hash
+    3. select * ... order by deduplication_hash, download the output as a CSV file, open in Excel, save as XLSX format
+    4. Content: in Excel,marks the 1 KEEP decision for each deduplication_hash group in that empty "fix_action" column
     5. Content: save Excel changes, return XLSX file to Systems, Systems will run the following script(s):
     6. Load Systems decisions in to the "fix_action" column appropriately. Anything not marked KEEP will be discarded.
-    7. Systems: where a normalized_code_id with duplicates has a row marked to KEEP - delete all other rows in group
-    8. Systems: where a normalized_code_id with duplicates has no row marked to KEEP - see Content, or keep 1 at random
+    7. Systems: where a deduplication_hash with duplicates has a row marked to KEEP - delete all other rows in group
+    8. Systems: where a deduplication_hash with duplicates has no row marked to KEEP - see Content, or keep 1 at random
     """
     # INFO log level leads to I/O overload due to httpx logging per issue, for 1000s of issues. At an arbitrary point in
     # processing, the error task overloads and experiences a TCP timeout, causing some number of errors to not be loaded
@@ -74,9 +75,7 @@ def identify_duplicate_code_id_values_in_v4(
 
     if (
         concept_map_uuid is None or
-        concept_map_version_uuid is None or
-        output_pkey_distinct_constraint_name is None or
-        output_table_name is None
+        concept_map_version_uuid is None
     ):
         LOGGER.warning("NO INPUT.")
         return
@@ -93,8 +92,8 @@ def identify_duplicate_code_id_values_in_v4(
     total_processed = 0
     LOGGER.warning(f"START: {datetime.datetime.now()} ")
 
-    # Set the destination table name, concept map UUID and concept map version UUID in count_query and select_query.
-    count_query = """
+    # Set the concept map UUID and concept map version UUID in query text.
+    count_goal_query = """
         select count(ctc.code) as goal from
         concept_maps.concept_map cm
         join concept_maps.concept_map_version cmv on cmv.concept_map_uuid = cm.uuid
@@ -103,6 +102,17 @@ def identify_duplicate_code_id_values_in_v4(
         join custom_terminologies.code ctc on sc.custom_terminology_uuid = ctc.uuid
         where cm.uuid = :concept_map_uuid
         and cmv.uuid = :concept_map_version_uuid
+        """
+    count_completed_query = """
+        select count(ctc.code) as completed from
+        concept_maps.concept_map cm
+        join concept_maps.concept_map_version cmv on cmv.concept_map_uuid = cm.uuid
+        join concept_maps.source_concept sc on sc.concept_map_version_uuid = cmv.uuid
+        join concept_maps.concept_relationship cr on cr.source_concept_uuid = sc.uuid
+        join custom_terminologies.code ctc on sc.custom_terminology_uuid = ctc.uuid
+        where cm.uuid = :concept_map_uuid
+        and cmv.uuid = :concept_map_version_uuid
+        and ctc.deduplication_hash is not null
         """
     select_query = f"""
         select cm.title, cm.uuid as cm_uuid, cmv.version as cm_version, 
@@ -121,21 +131,33 @@ def identify_duplicate_code_id_values_in_v4(
         order by ctc.uuid
         limit :page_size
         """
+    insert_query = f"""
+        insert into custom_terminologies.code
+        (
+            deduplication_hash
+        )
+        values 
+        (
+            :deduplication_hash
+        )
+        where uuid = :code_uuid
+        """
+
     from app.database import get_db
     conn = get_db()
     try:
-        count1 = conn.execute(
-            text(count_query),
+        count_goal = conn.execute(
+            text(count_goal_query),
             {
                 "concept_map_uuid": concept_map_uuid,
                 "concept_map_version_uuid": concept_map_version_uuid,
             }
         ).first()
-        LOGGER.warning(f"Rows in v4 table: {count1}")
-        count2 = conn.execute(
-            text(f"select count(*) from {output_table_name}")
+        LOGGER.warning(f"Rows in v4 table: {count_goal.goal}")
+        count_completed = conn.execute(
+            text(count_completed_query)
         ).first()
-        LOGGER.warning(f"Output rows completed before this run: {count2}")
+        LOGGER.warning(f"Output rows completed before this run: {count_completed.completed}")
     except Exception as e:
         conn.rollback()
         raise e
@@ -171,28 +193,16 @@ def identify_duplicate_code_id_values_in_v4(
                         code_string,
                         rejected
                     ) = prepare_dynamic_value_for_sql_issue(row.code, row.display)
-                    normalized_code_id = generate_code_id(  # Not supporting depends on data at the moment
+                    deduplication_hash = generate_code_id(  # Not supporting depends on data at the moment
                         code_string=code_string,
                         display=json.loads(code_string).get("text", ""),
                     )
-
-                    insert_query = f"""
-                    insert into custom_terminologies.code
-                    (
-                        normalized_code_id
-                    )
-                    values 
-                    (
-                        :normalized_code_id
-                    )
-                    where uuid = :code_uuid
-                    """
 
                     try:
                         conn.execute(
                             text(insert_query),
                             {
-                                "normalized_code_id": normalized_code_id,
+                                "deduplication_hash": deduplication_hash,
                                 "code_uuid": row.custom_terminologies_code_uuid
                             }
                         )
@@ -200,12 +210,7 @@ def identify_duplicate_code_id_values_in_v4(
 
                     except Exception as e:
                         conn.rollback()
-                        error_summary = message_exception_summary(e)
-                        # already processed this row? if so, just skip it
-                        if output_pkey_distinct_constraint_name not in error_summary:
-                            raise e
-
-                    # for row in result
+                        raise e
 
         except Exception as e:
             conn.rollback()
@@ -214,8 +219,6 @@ def identify_duplicate_code_id_values_in_v4(
             LOGGER.warning("HALTED due to ERROR.")
             done = True
 
-        # while not done
-
     LOGGER.warning("DONE.")
 
 
@@ -223,12 +226,10 @@ def identify_v4_concept_map_duplicates():
     """
     Command line endpoint for identifying v4 duplicate check locally. Does not support command line inputs at this time.
     """
-    # todo: For this command line endpoint, add a line for each table to be checked.
-    identify_duplicates_for_v4_concept_map(
+    # todo: For this command line endpoint, call for each table to be checked. data_migration.py lists UUID
+    identify_duplicate_code_values_in_v4(
         "c504f599-6bf6-4865-8220-bb199e3d1809",
-        "955e518a-8030-4fe5-9e61-e0a6e6fda1b3",
-        "custom_terminologies.test_code_condition_duplicates_w_id",
-        "test_code_condition_duplicates_w_id_pkey"
+        "955e518a-8030-4fe5-9e61-e0a6e6fda1b3"
     )
 
 
