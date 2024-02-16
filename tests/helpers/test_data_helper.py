6d235b5058d81d21b986eaea59ca53ad7e85fc60
Susan Korgen <susan@projectronin.com>
2024-02-16 13:51:07 -0500

    SQL input unit tests (#438)

--- tests/helpers/test_data_helper.py
+++ tests/helpers/test_data_helper.py
@@ -4,7 +4,7 @@ from _pytest.python_api import raises
 
 from app.database import get_db
 from app.app import create_app
-from app.helpers.data_helper import hash_string
+from app.helpers.data_helper import hash_string, escape_sql_input_value
 
 
 class DataMigrationTests(unittest.TestCase):
@@ -27,3 +27,20 @@ class DataMigrationTests(unittest.TestCase):
         normalized_code = '{"coding":[{"code":292241000119108,"system":"http://snomed.info/sct"},{"code":"G57.61","system":"http://hl7.org/fhir/sid/icd-10-cm"}],"text":"Morton\'s neuroma of right foot"}'
         hashed_code = hash_string(normalized_code)
         assert hashed_code == "1f1d59b2559e305ca874a0a66726796a"
+
+    def test_escape_sql_input_value_no_change(self):
+        input_value = '{"coding":[{"code":292241000119108,"system":"http://snomed.info/sct"},{"code":"G57.61","system":"http://hl7.org/fhir/sid/icd-10-cm"}],"text":"Mortons neuroma of right foot"}'
+        test_result = escape_sql_input_value(input_value)
+        assert test_result == input_value
+
+    def test_escape_sql_input_value_single_quote(self):
+        input_value = '{"coding":[{"code":292241000119108,"system":"http://snomed.info/sct"},{"code":"G57.61","system":"http://hl7.org/fhir/sid/icd-10-cm"}],"text":"Morton\'s neuroma of right foot"}'
+        sql_escaped = '{"coding":[{"code":292241000119108,"system":"http://snomed.info/sct"},{"code":"G57.61","system":"http://hl7.org/fhir/sid/icd-10-cm"}],"text":"Morton\'\'s neuroma of right foot"}'
+        test_result = escape_sql_input_value(input_value)
+        assert test_result == sql_escaped
+
+    def test_escape_sql_input_value_colon_confused_with_sql_binding(self):
+        input_value = '{"coding": [{"code": "271350002", "display": "Urine microscopy: leukocytes present (finding)", "system": "http://snomed.info/sct"}, {"code": "791.7", "display": "Urine micr.:leukocytes present", "system": "http://hl7.org/fhir/sid/icd-9-cm/diagnosis"}, {"code": "R82.81", "display": "Urine micr.:leukocytes present", "system": "urn:oid:2.16.840.1.113883.6.90"}], "text": "Urine micr.:leukocytes present"}'
+        sql_escaped = '{"coding": [{"code": "271350002", "display": "Urine microscopy: leukocytes present (finding)", "system": "http://snomed.info/sct"}, {"code": "791.7", "display": "Urine micr.\:leukocytes present", "system": "http://hl7.org/fhir/sid/icd-9-cm/diagnosis"}, {"code": "R82.81", "display": "Urine micr.\:leukocytes present", "system": "urn:oid:2.16.840.1.113883.6.90"}], "text": "Urine micr.\:leukocytes present"}'
+        test_result = escape_sql_input_value(input_value)
+        assert test_result == sql_escaped
